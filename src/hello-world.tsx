f9b54e30719f07a2a7f29a5b57568fd3b67479a3
ojaros <oliverjaros214@gmail.com>
2022-06-30 11:02:46 -0500

    added tailwind configuration to the project, if getting errors make sure you are running the latest version of node

--- src/hello-world.tsx
+++ src/hello-world.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 class HelloWorld extends React.Component {
   render() {
-    return <h1>Hello, Luke!!</h1>;
+    return <h1 className="text-3xl font-bold underline">Hello, Luke!!</h1>;
   }
 }
 
