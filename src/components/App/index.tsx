43aaf1d5176609f362f92e958abca23f824ee136
ojaros <oliverjaros214@gmail.com>
2022-07-01 12:55:52 -0500

    created navbar

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './index.css';
 import Hello from '../../hello-world';
+import Nav from '../Nav'
 export {};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Hello></Hello>
+    <div>
+      <header>
+        <Nav></Nav>
       </header>
+      <div className='p-4'>
+        <Hello></Hello>
     </div>
+    </div>
+
   );
 }
 
