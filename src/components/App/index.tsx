4adb2c18e16c04068ece9fbc885932ac0bcaf246
ojaros <oliverjaros214@gmail.com>
2022-07-01 20:07:52 -0500

    first draft of top section on landing page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './index.css';
-import Hello from '../../hello-world';
 import Nav from '../Nav'
+import Welcome from '../Welcome'
 export {};
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
       <header>
         <Nav></Nav>
       </header>
-      <div className='p-4'>
-        <Hello></Hello>
-    </div>
+      <main className="flex-grow">
+        <Welcome></Welcome>
+      </main>
+
     </div>
 
   );
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -9,35 +9,19 @@ export default class Nav extends React.Component {
                 relative
                 w-full
                 flex flex-wrap
-                items-center
-                justify-between
+                justify-center
                 p-4
-                mx-1
-                bg-gray-100
+                h-32
+                bg-gradient-to-b from-[#45315F] to-transparent
                 text-gray-500
                 hover:text-gray-700
                 focus:text-gray-700
-                shadow-lg
                 navbar navbar-expand-lg navbar-light
             ">
-                <div className="ml-32">
+                <div className="">
                     <img alt="Drem logo" className="h-7" src="../../images/logos/Drem-typeface-blk.png"></img>
                 </div>
 
-                <div className="mr-32">
-                    <a className="mx-4" href="/product">
-                        Product
-                    </a>
-                    <a className="mx-4" href="/about">
-                        About Us
-                    </a>
-                    <a className="mx-4" href="/contact">
-                        Contact
-                    </a>
-                    <button className="mx-4 bg-gradient-to-r from-[#A814AB] to-blue-500 hover:from-blue-500 hover:to-[#A814AB] text-white py-2 px-4 rounded-full">
-                        Enter App
-                    </button>
-                </div>
             </nav>
         )
     }
