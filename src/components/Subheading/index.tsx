4d5da86abbb9fdc017d2a2c7cce1083ec23b16ac
ojaros <oliverjaros214@gmail.com>
2022-07-05 16:40:49 -0500

    created Subheading component. Use props to pass text and image to each instance of the component

---
+++ src/components/Subheading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function Subheading(props: any) {
+    return (
+        <div>
+            <h4>{props.text}</h4>
+            <image>{props.image}</image>
+        </div>
+    )
+}
+
+export default Subheading
