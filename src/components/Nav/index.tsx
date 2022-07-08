9ad72e069d6b94d378eb46d68fd8d4bf6016d093
Jodi Chui <jodychui7@gmail.com>
2022-07-08 16:31:11 -0700

    Adjusted the Navbar
    Shortened the gradient length, increased Drem typeface font size to 64pt, changed the gradient color from-[#b1b7cd] to-transparent.

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,21 +5,25 @@ export default class Nav extends React.Component {
 
     render() {
         return (
-            <nav className="
-                relative
-                w-full
-                flex flex-wrap
-                justify-center
-                p-4
-                h-32
-                bg-gradient-to-b from-[#45315F] to-transparent
-                navbar navbar-expand-lg navbar-light
-            ">
-                <div className="">
-                    <h4 className="text-2xl font-damion text-gray1">Drem</h4>
-                </div>
+            // <div className="">
+                <nav className="
+                                relative
+                                w-full
+                                flex flex-wrap
+                                justify-center
+                                h-20
+                                p-1
+                                bg-gradient-to-b from-[#b1b7cd] to-transparent 
+                                navbar navbar-expand-lg navbar-light
+                                ">
 
-            </nav>
+                    <div className="">
+                        <h4 className="text-[64px] font-damion text-gray1">Drem</h4>
+                    </div>
+
+                </nav>
+            // </div>
+            
         )
     }
 }
