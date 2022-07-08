a46c27f62dd1cc072418c2c91f9f46f42b82e40c
Jodi Chui <jodychui7@gmail.com>
2022-07-08 15:17:44 -0700

    Implemented Subheading Component
    Added the title, description, and image in each subheading along with the line across the page.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -7,12 +7,6 @@ import Subheading from '../Subheading'
 import Footer from '../Footer';
 export {};
 
-// function splitLine(props: any) {
-//   const text = props.text;
-//   const newText = text.split('\n').map((str: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined) => <p>{str}</p>);
-//   return newText;
-// }
-
 function App() {
   return (
     <div className="w-screen">
@@ -27,18 +21,18 @@ function App() {
 
       <div className="relative bg-white shadow-[0_-8px_6px_6px_rgba(0,0,0,0.25)] z-30">
 
-        
         <div className="text-left pt-[200px]">
-          <Subheading title={'Find Your\nCommunity'} text={'Take your first step in web3, together; with\nDrem, the more you engage with your\ncommunity, the more you earn'} image={'../../images/sc-ui/post-sc-1.png'} img_pos={"float-right mr-[123px] pt-[373px]"} width={"920"} height={"377.03"} text_pos={"w-1/3 ml-[123px] pt-[373px]"} title_pos={"mb-[25px]"}/>
+          <Subheading title={'Find Your Community'} text={'Take your first step in web3, together; with Drem, the more you engage with your community, the more you earn'} image={'../../images/sc-ui/post-sc-1.png'} img_pos={"float-right mr-[123px] pt-[373px]"} width={"920"} height={"377.03"} text_pos={"w-1/3 ml-[123px] pt-[373px]"} title_pos={"mb-[25px]"}/>
         </div>
         
-        <div className="text-right relative top-0 right-0">
-          <Subheading title={'Share Your\nJourney'} text={'Explore web3 in one simple\ninterface; own, learn, and share every\nstep of your experience'} image={'./images/sc-ui/user-strats-sc-1.png'} img_pos={"float-left align-middle ml-[212px] mr-[17px]"} width={"879"} height={"424"} text_pos={"place-content-end -w-1/8 mr-[204px] pb-[100px]"} title_pos={"mb-[25px]"}/>
+        <div className="text-right">
+          <Subheading title={'Share Your Journey'} text={'Explore all activity in one simple interface; own, learn, and share every step of your experience'} image={'./images/sc-ui/user-strats-sc-1.png'} img_pos={"float-left align-middle ml-[212px] mr-[17px]"} width={"879"} height={"424"} text_pos={"place-content-end -w-1/8 mr-[204px] pb-[100px]"} title_pos={"mb-[25px]"}/>
+        </div>
+        
+        <div className="text-left">
+          <Subheading title={'Build Your Frontier'} text={'Create or mirror the best investment strategies; you don\'t need to be an expert to invest like one'} image={'images/sc-ui/portfolio-sc-2.png'} img_pos={"float-right align-middle mr-[250px] -mt-[168px] "} width={"541"} height={"681.87"} text_pos={"w-1/3 ml-[250px]"} title_pos={"mb-[25px]"}/>
         </div>
 
-        <div className="text-left">
-          <Subheading title={'Build Your\nFrontier'} text={'Create or mirror the best investment\nstrategies; you don\'t need to be an\nexpert to invest like one'} image={'images/sc-ui/portfolio-sc-1.png'} img_pos={"float-right align-middle mr-[250px] -mt-[168px] "} width={"541"} height={"681.87"} text_pos={"w-1/3 ml-[250px]"} title_pos={"mb-[25px]"}/>
-        </div>
         <Footer></Footer>
       </div>
 
--- src/components/Subheading/index.tsx
+++ src/components/Subheading/index.tsx
@@ -7,16 +7,17 @@ function Subheading(props: any) {
             <img alt="Subheader Img" className={props.img_pos} width={props.width} height={props.height} src={props.image}></img>           
                 <div className={props.text_pos}> 
                     <div className = {props.title_pos}>
-                        <h3 className="text-8xl leading-[96px] pb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#A814AB] to-[#0029FF]">
+                        <h3 className="text-8xl leading-[96px] pb-2 pr-2 text-transparent bg-clip-text bg-gradient-to-r from-[#A814AB] to-[#0029FF]">
                             {props.title}
+                            {/* displays title of subheading */}
                         </h3>
                     </div>
-
                     <h4 className="text-[32px] leading-[48px] text-[#06080F]">{props.text}</h4>
+                    {/* displays description of subheading */}
                 </div>
 
-
-            <div className="w-[639px] border-t-[1px] border-[#7A7A7A] mx-auto my-[300px]"></div> {/* to test */}
+            <div className="w-[639px] border-t-[1px] border-[#7A7A7A] mx-auto my-[300px]"></div> 
+            {/* creates the line/boarder to separate subheadings */}
         
         </div>
     )
