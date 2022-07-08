86676d95962066a3c089fa69b091adc58a96b959
Jodi Chui <jodychui7@gmail.com>
2022-07-08 15:30:19 -0700

    Updated Subheading Component
    Moved the first subheader text block to the right

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -22,13 +22,13 @@ function App() {
       <div className="relative bg-white shadow-[0_-8px_6px_6px_rgba(0,0,0,0.25)] z-30">
 
         <div className="text-left pt-[200px]">
-          <Subheading title={'Find Your Community'} text={'Take your first step in web3, together; with Drem, the more you engage with your community, the more you earn'} image={'../../images/sc-ui/post-sc-1.png'} img_pos={"float-right mr-[123px] pt-[373px]"} width={"920"} height={"377.03"} text_pos={"w-1/3 ml-[123px] pt-[373px]"} title_pos={"mb-[25px]"}/>
+          <Subheading title={'Find Your Community'} text={'Take your first step in web3, together; with Drem, the more you engage with your community, the more you earn'} image={'../../images/sc-ui/post-sc-1.png'} img_pos={"float-right mr-[123px] pt-[373px]"} width={"920"} height={"377.03"} text_pos={"w-1/3 ml-[140px] pt-[373px]"} title_pos={"mb-[25px]"}/>
         </div>
         
         <div className="text-right">
           <Subheading title={'Share Your Journey'} text={'Explore all activity in one simple interface; own, learn, and share every step of your experience'} image={'./images/sc-ui/user-strats-sc-1.png'} img_pos={"float-left align-middle ml-[212px] mr-[17px]"} width={"879"} height={"424"} text_pos={"place-content-end -w-1/8 mr-[204px] pb-[100px]"} title_pos={"mb-[25px]"}/>
         </div>
-        
+
         <div className="text-left">
           <Subheading title={'Build Your Frontier'} text={'Create or mirror the best investment strategies; you don\'t need to be an expert to invest like one'} image={'images/sc-ui/portfolio-sc-2.png'} img_pos={"float-right align-middle mr-[250px] -mt-[168px] "} width={"541"} height={"681.87"} text_pos={"w-1/3 ml-[250px]"} title_pos={"mb-[25px]"}/>
         </div>
