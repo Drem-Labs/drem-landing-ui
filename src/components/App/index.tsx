a6015a4314bf2570be64e73158d28a6d279cd767
Ali Alaoui <greenbarca2001@gmail.com>
2022-07-16 13:23:36 +0100

    Changed component sizes for multiple screens.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -14,33 +14,33 @@ function App() {
     <div className="w-screen">
 
       <div className="fixed inset-x-0 top-0 z-30">
-        <Nav></Nav>  
+        <Nav></Nav>
       </div>
 
       <div className="w-screen">
-        <Hero></Hero>  
+        <Hero></Hero>
       </div>
 
       <div className="relative bg-white shadow-[0_-8px_6px_6px_rgba(0,0,0,0.25)] z-30">
 
-        <div className="text-left pt-[200px]">
-          <Subheading title={'Find Your Community'} text={'Take your first step in web3, together; with Drem, the more you engage with your community, the more you earn'} image={'../../images/sc-ui/post-sc-1.png'} img_pos={"float-right mr-[156px] pt-[373px]"} width={"920"} height={"377.03"} text_pos={"w-1/3 ml-[187px] pt-[373px]"} title_pos={"mb-[25px]"} line_pos={"my-[300px]"}/>
+        <div className="text-left 2xl:pt-[200px]">
+          <Subheading title={'Find Your Community'} text={'Take your first step in web3, together; with Drem, the more you engage with your community, the more you earn'} image={'../../images/sc-ui/post-sc-1.png'} img_pos={"float-right mr-[78px] 2xl:mr-[156px] pt-[200px] 2xl:pt-[373px]"} width={"640 2xl:920"} height={"377.03"} text_pos={"w-1/3 ml-[150px] 2xl:ml-[187px] pt-[150px] 2xl:pt-[373px]"} title_pos={"mb-[25px]"} line_pos={"my-[180px] 2xl:my-[300px]"}/>
         </div>
-        
-         
+
+
         <div className="text-right">
-          <Subheading title={'Share Your Journey'} text={'Explore all activity in one simple interface; own, learn, and share every step of your experience'} image={'./images/sc-ui/user-strats-sc-1.png'} img_pos={"float-left align-middle ml-[212px] mr-[17px]"} width={"879"} height={"424"} text_pos={"place-content-end -w-1/8 mr-[209px] pb-[100px]"} title_pos={"mb-[25px]"} line_pos={"my-[230px]"}/>
+          <Subheading title={'Share Your Journey'} text={'Explore all activity in one simple interface; own, learn, and share every step of your experience'} image={'./images/sc-ui/user-strats-sc-1.png'} img_pos={"float-left align-middle ml-[130px] 2xl:ml-[212px] mr-[40px]"} width={"640 2xl:879"} height={"424"} text_pos={"place-content-end -w-1/8 mr-[130px] 2xl:mr-[209px] pb-[100px]"} title_pos={"mb-[25px]"} line_pos={"my-[220px] 2xl:my-[230px]"}/>
         </div>
 
         <div className="text-left">
-          <Subheading title={'Build Your Frontier'} text={'Create or mirror the best investment strategies; you don\'t need to be an expert to invest like one'} image={'images/sc-ui/portfolio-sc-2.png'} img_pos={"float-right align-middle mr-[313px] -mt-[140px]"} width={"541"} height={"681.87"} text_pos={"w-1/3 ml-[312px]"} title_pos={"mb-[25px] mt-[100px]"} line_pos={"my-[400px]"}/>
+          <Subheading title={'Build Your Frontier'} text={'Create or mirror the best investment strategies; you don\'t need to be an expert to invest like one'} image={'images/sc-ui/portfolio-sc-2.png'} img_pos={"float-right align-middle mr-[313px] mt-[40px] 2xl:mt-[140px]"} width={"300 2xl:541"} height={"681.87"} text_pos={"w-1/3 ml-[312px]"} title_pos={"mb-[25px] mt-[100px]"} line_pos={"my-[200px] 2xl:my-[400px]"}/>
         </div>
 
         <Footer></Footer>
       </div>
 
     </div>
-    
+
 
   );
 }
--- src/components/Subheading/index.tsx
+++ src/components/Subheading/index.tsx
@@ -3,15 +3,15 @@ import './index.css';
 
 function Subheading(props: any) {
     return (
-        <div className="General-text font-semibold not-italic -tracking-[1px] text-clip">  
+        <div className="General-text font-semibold not-italic -tracking-[1px] text-clip">
 
-            <img alt="Subheader Img" className={props.img_pos} width={props.width} height={props.height} src={props.image}></img>           
-                
-                <div className={props.text_pos}> 
+            <img alt="Subheader Img" className={props.img_pos} width={props.width} height={props.height} src={props.image}></img>
+
+                <div className={props.text_pos}>
 
                     <div className = {props.title_pos}>
 
-                        <h3 className="text-8xl leading-[96px] pb-2 pr-2 text-transparent bg-clip-text bg-gradient-to-r from-[#A814AB] to-[#0029FF]">
+                        <h3 className="text-7xl 2xl:text-8xl leading-[96px] pb-2 pr-2 text-transparent bg-clip-text bg-gradient-to-r from-[#A814AB] to-[#0029FF]">
                             {props.title}
                             {/* displays title of subheading */}
                         </h3>
@@ -23,13 +23,13 @@ function Subheading(props: any) {
 
                 </div>
 
-            
+
             <div className={props.line_pos}>
-                <div className="w-[639px] border-t-[1px] border-[#7A7A7A] mx-auto"></div> 
+                <div className="w-[639px] border-t-[1px] border-[#7A7A7A] mx-auto"></div>
             </div>
             {/* creates the line/boarder to separate subheadings */}
             {/* each subheading has different sizes so use line_pos to make each subheading section the same size */}
-        
+
         </div>
     )
 }
