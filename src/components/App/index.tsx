278a385b7ef6ff72ad95c7852a01c723cbbeb944
Ali Alaoui <greenbarca2001@gmail.com>
2022-07-18 16:12:04 +0100

    Made subheader content mobile-compatible.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -28,12 +28,12 @@ function App() {
                       title={'Find Your Community'}
                       text={'Take your first step in web3, together; with Drem, the more you engage with your community, the more you earn'}
                       image={'../../images/sc-ui/post-sc-1.png'}
-                      img_pos={"object-contain mr-96 pl-24 pt-[150px] 2xl:pt-[373px]"}
-                      width={"817 2xl:1225"}
-                      height={"217 2xl:433"}
-                      text_pos={"sm:w-1/3 ml-3 sm:ml-[150px] 2xl:ml-[187px] pt-[150px] 2xl:pt-[373px]"}
+                      img_pos={"object-contain sm:mr-96 sm:pl-24 pt-4 sm:pt-[150px] 2xl:pt-[373px]"}
+                      width={"817"}
+                      height={"217"}
+                      text_pos={"sm:w-1/3 ml-3 sm:ml-[150px] 2xl:ml-[187px] pt-4 sm:pt-[150px] 2xl:pt-[373px]"}
                       title_pos={"mb-[25px]"}
-                      line_pos={"my-[180px] 2xl:my-[300px]"}
+                      line_pos={"my-[90px] sm:my-[180px] 2xl:my-[300px]"}
           />
         </div>
 
@@ -43,12 +43,12 @@ function App() {
                       title={'Share Your Journey'}
                       text={'Explore all activity in one simple interface; own, learn, and share every step of your experience'}
                       image={'./images/sc-ui/user-strats-sc-1.png'}
-                      img_pos={"object-contain ml-[130px] 2xl:ml-[212px] mr-[40px]"}
-                      width={"640 2xl:879"}
+                      img_pos={"object-contain sm:ml-[130px] 2xl:ml-[212px] mr-[40px]"}
+                      width={"640"}
                       height={"424"}
-                      text_pos={"place-content-end -w-1/8 mr-[130px] 2xl:mr-[209px] pb-[30px]"}
+                      text_pos={"sm:w-1/8 ml-3 sm:mr-[130px] 2xl:mr-[209px] pb-[30px]"}
                       title_pos={"mb-[25px]"}
-                      line_pos={"my-[220px] 2xl:my-[230px]"}
+                      line_pos={"my-[90px] sm:my-[220px] 2xl:my-[230px]"}
           />
         </div>
 
@@ -57,12 +57,10 @@ function App() {
                       title={'Build Your Frontier'}
                       text={'Create or mirror the best investment strategies; you don\'t need to be an expert to invest like one'}
                       image={'images/sc-ui/portfolio-sc-2.png'}
-                      img_pos={"object-contain float-right pl-24 mt-[40px] 2xl:mt-[120px]"}
-                      width={"541"}
-                      height={"681.87"}
-                      text_pos={"w-1/3 ml-[250px]"}
+                      img_pos={"w-[300px] sm:w-[541px] object-contain pl-24 mt-[40px] 2xl:mt-[120px]"}
+                      text_pos={"sm:w-1/3 mx-3 sm:ml-[250px]"}
                       title_pos={"mb-[25px] mt-[100px]"}
-                      line_pos={"my-[200px] 2xl:my-[400px]"}
+                      line_pos={"my-[90px] sm:my-[200px] 2xl:my-[400px]"}
           />
         </div>
         <Footer></Footer>
