afc6a8ad4e3aa2bbe9baba75f3193d1639514e5c
Patrick <patricklee260@gmail.com>
2022-07-20 22:16:29 -0700

    adjusted footer breakpoints

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ class Footer extends React.Component {
   styling = {
     div: "text-center mx-15",
     form: "",
-    input: "rounded-[10px] mt-[54px] bg-[#ebebeb99] w-64 sm:w-[763px] md:w-5/6 xl:w-[763px] h-12 sm:h-[63px] text-center",
+    input: "rounded-[10px] mt-[54px] bg-[#ebebeb99] w-64 sm:w-2/3 md:w-5/6 xl:w-[763px] h-12 sm:h-[63px] text-center",
     button: "py-[12px] sm:py-[24px] px-[20px] sm:px-[70px] m-auto mt-8 sm:mt-[67px] text-transparent bg-clip-text bg-gradient-to-r from-[#A814AB] to-[#0029FF] border-[#F0F0F0] border-[1px] rounded-[50px] text-center sm:text-[32px] leading-[20px] tracking-[0.25px] font-bold shadow-[0_8px_10px_-1px_rgba(0,0,0,0.35)]",
     header: ""
   }
@@ -24,12 +24,15 @@ class Footer extends React.Component {
                          sm:mt-32
                          2xl:mt-[258px]
                          text-4xl
-                         sm:text-6xl
+                         sm:text-3xl
+                         md:text-4xl
+                         lg:text-6xl
+                         2xl:text-8xl
                          leading-[96px]
                          text-transparent
                          bg-clip-text
                          bg-gradient-to-r from-[#A814AB] to-[#0029FF]">Join Us</h1>
-          <div className="pt-8 sm:text-4xl leading-4 sm:leading-[48px] text-[#06080F]">
+          <div className="pt-8 leading-4 sm:text-sm sm:text-base md:text-lg lg:text-3xl 2xl:text-5xl text-[#06080F]">
             <p>Become a part of the community creating</p>
             <p>the future. It's yours, own it 💎</p>
           </div>
